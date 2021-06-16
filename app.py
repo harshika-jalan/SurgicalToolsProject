883d18a4e283b68cf62369aed291eaefe4b5cca6
Harshika Jalan <harshikajalan@berkeley.edu>
2021-06-16 11:48:36 +0530

    image stitching sort of working

--- app.py
+++ app.py
@@ -390,35 +390,73 @@ def check(name, response):
     return missingInstruments, wrongInstruments
 
 #Function for image stitching (2 images)
-def stitch(img_, img) :
-    img_= img_.numpy()
-    img = img.numpy()
-    img1 = cv2.cvtColor(img_,cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sift = cv2.xfeatures2d.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+def stitch(img1, img2) :
+    #img1= cv2.imread(os.path.join(os.getcwd(), first))
+    #img2 = cv2.imread(os.path.join(os.getcwd(), second))
+    img1 = img1.numpy()
+    img2 = img2.numpy()
+    img1_gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    img2_gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    # Create our ORB detector and detect keypoints and descriptors
+    orb = cv2.ORB_create(nfeatures=2000)
 
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1,des2, k=2)
-    # Apply ratio test
+    # Find the key points and descriptors with ORB
+    keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
+
+    # Create a BFMatcher object.
+    # It will find all of the matching keypoints on two images
+    bf = cv2.BFMatcher_create(cv2.NORM_HAMMING)
+
+    # Find matching points
+    matches = bf.knnMatch(descriptors1, descriptors2,k=2)
+
+
+    # Finding the best matches
     good = []
-    for m in matches:
-        if m[0].distance < 0.5*m[1].distance:
+    for m, n in matches:
+        if m.distance < 0.6 * n.distance:
             good.append(m)
-    matches = np.asarray(good)
-    if len(matches[:,0]) >= 4:
-        src = np.float32([ kp1[m.queryIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
-        dst = np.float32([ kp2[m.trainIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
-        H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
-        #print H
-    else:
-        raise AssertionError("Cannot find enough keypoints.")
-    dst = cv2.warpPerspective(img_,H,(img.shape[1] + img_.shape[1], img.shape[0]))
-    dst[0:img.shape[0], 0:img.shape[1]] = img
-    return dst
+    # Set minimum match condition
+    #MIN_MATCH_COUNT = 10
 
+    #if len(good) > MIN_MATCH_COUNT:
+        # Convert keypoints to an argument for findHomography
+    src_pts = np.float32([ keypoints1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    dst_pts = np.float32([ keypoints2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+    # Establish a homography
+    M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+
+    result = warpImages(img2, img1, M)
+
+    return result
+
+def warpImages(img1, img2, H):
+
+  rows1, cols1 = img1.shape[:2]
+  rows2, cols2 = img2.shape[:2]
+
+  list_of_points_1 = np.float32([[0,0], [0, rows1],[cols1, rows1], [cols1, 0]]).reshape(-1, 1, 2)
+  temp_points = np.float32([[0,0], [0,rows2], [cols2,rows2], [cols2,0]]).reshape(-1,1,2)
+
+  # When we have established a homography we need to warp perspective
+  # Change field of view
+  list_of_points_2 = cv2.perspectiveTransform(temp_points, H)
+
+  list_of_points = np.concatenate((list_of_points_1,list_of_points_2), axis=0)
+
+  [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
+  [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
+
+  translation_dist = [-x_min,-y_min]
+
+  H_translation = np.array([[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
+
+  output_img = cv2.warpPerspective(img2, H_translation.dot(H), (x_max-x_min, y_max-y_min))
+  output_img[translation_dist[1]:rows1+translation_dist[1], translation_dist[0]:cols1+translation_dist[0]] = img1
+
+  return output_img
 
 if __name__ == '__main__':
     #app.run(debug=True, host = '0.0.0.0', port=5000)
